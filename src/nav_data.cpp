8888f30c1be464fcc42401f4f21c623b70a87405
Michael Koval <koval.michael@gmail.com>
2010-12-03 20:14:27 -0500

    Reformatted the nav_data with a  consistent style

--- src/nav_data.cpp
+++ src/nav_data.cpp
@@ -2,19 +2,19 @@
 
 C_RESULT navdata_init( void* private_data )
 {
-  //Your code to initialize your local variables.
-  return C_OK;
+	//Your code to initialize your local variables.
+	return C_OK;
 }
 
 C_RESULT navdata_process( const navdata_unpacked_t* const pnd )
 {
-  //Retrieves current Navdata unpacked.
-  printf("%d\n",pnd->navdata_demo.vbat_flying_percentage);
-  return C_OK;
+	//Retrieves current Navdata unpacked.
+	printf("%d\n",pnd->navdata_demo.vbat_flying_percentage);
+	return C_OK;
 }
 
 C_RESULT navdata_release( void )
 {
-  //Free local variables.
-  return C_OK;
+	//Free local variables.
+	return C_OK;
 }
